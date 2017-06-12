b20c3a5a97c2bc5433f7f26a5debe70d995cb956
zhangyun <zhangyun@zhangyun-pc-2.local>
2017-06-12 21:53:51 +0800

    sync io

---
+++ src/TimeClient.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by zhangyun on 2017/6/12.
+ */
+public class TimeClient {
+    public static void main(String[] args){
+        int port = 8081;
+        if (args != null && args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+
+            }
+        }
+
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+
+        try {
+            socket = new Socket("127.0.0.1",port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(),true);
+            out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed");
+            String resp = in.readLine();
+            System.out.println("Now is :" + resp);
+        }catch (Exception e){
+
+        }finally {
+            if (out != null){
+                out.close();
+                out = null;
+            }
+
+            if (in != null){
+                try {
+                    in.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            if (socket != null){
+                try {
+                    socket.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+                socket = null;
+            }
+        }
+    }
+}
---
+++ src/TimeServer.java
@@ -0,0 +1,39 @@
+/**
+ * Created by zhangyun on 2017/6/12.
+ */
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class TimeServer {
+
+    public  static void main(String[] args) throws IOException {
+        int port = 8081;
+        if (args != null && args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+
+            }
+        }
+
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port :" + port);
+            Socket socket = null;
+            while (true){
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        }finally {
+            if (server != null){
+                System.out.println("The time server close");
+                server.close();
+                server = null;
+            }
+        }
+    }
+}
